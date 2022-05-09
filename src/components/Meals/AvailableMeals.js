19a6614c440f50dc67e8a3f7d35282dc5809e7b4
Aly Badawy <AlyBadawy@users.noreply.github.com>
2022-05-09 09:19:25 -0400

    Fetching Meals from FireBase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,12 +1,46 @@
 import React from "react";
-import dummyMeals from "./DummyMeals";
+// import dummyMeals from "./DummyMeals";
 import Card from "../UI/Card";
 import MealItem from "./MealItem";
 
 import style from "./AvailableMeals.module.sass";
 
 const AvailableMeals = () => {
-  const mealsList = dummyMeals.map((meal) => {
+  const [meals, setMeals] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://alybadawy-8b6ed-default-rtdb.firebaseio.com/meals"
+      );
+      const responseData = await response.json();
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+
+    fetchMeals();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={style.MealsLoading}>
+        <p>Loading</p>
+      </section>
+    );
+  }
+
+  const mealsList = meals.map((meal) => {
     return <MealItem key={meal.id} meal={meal} />;
   });
   return (
