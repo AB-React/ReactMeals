d7079d6077074fd961b94b7cd5e5199eccc99411
Aly Badawy <AlyBadawy@users.noreply.github.com>
2022-05-09 09:42:10 -0400

    Handling Errors

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -8,12 +8,18 @@ import style from "./AvailableMeals.module.sass";
 const AvailableMeals = () => {
   const [meals, setMeals] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
+  const [httpError, setHttpError] = React.useState(null);
 
   React.useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
-        "https://alybadawy-8b6ed-default-rtdb.firebaseio.com/meals"
+        "https://alybadawy-8b6ed-default-rtdb.firebaseio.com/meals.json"
       );
+
+      // if (!response.ok) {
+      //   throw new Error("Something went wrong!");
+      // }
+
       const responseData = await response.json();
       const loadedMeals = [];
 
@@ -29,13 +35,33 @@ const AvailableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals();
+    // fetchMeals();
+
+    // try {
+    //   fetchMeals();
+    // } catch (error) {
+    //   setIsLoading(false);
+    //   setHttpError(error.message);
+    // }
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
   if (isLoading) {
     return (
       <section className={style.MealsLoading}>
         <p>Loading</p>
+      </section>
+    );
+  }
+
+  if (!!httpError) {
+    return (
+      <section className={style.MealsError}>
+        <p>{httpError}</p>
       </section>
     );
   }
